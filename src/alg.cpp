47f59cc5141ac6ec3fc68ac34849a037c8c642f8
shattova <116936426+shattova@users.noreply.github.com>
2023-03-10 22:03:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,25 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int l = 0;
+  int r = 0;
+  int result = 0;
+  int mid;
+  while (l <= r) {
+    mid = (l + r) / 2;
+    if (arr[mid] == value) {
+      for (int i = 0; arr[mid] == arr[mid + 1]; i++) {
+        result++;
+      }
+      for (int i = 1; arr[mid] == arr[mid - 1]; i++) {
+        result++;
+      }
+    } else if (arr[mid] > value) {
+      r = mid - 1;
+    }else {
+      l = mid + 1;
+    }
+    return result;
+  }
+  return 0;
 }
